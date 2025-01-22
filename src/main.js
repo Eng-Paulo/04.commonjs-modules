6269f173fa15d126c610fe7e49697c1c2a6e80de
Eng-Paulo <pauloroger.dev@gmail.com>
2025-01-21 21:07:08 -0300

    Aulas

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const p = require("./services/products");
+
+async function main() {
+    console.log('carinhos de compras');
+    p.getFullName("404", "mousepad");
+    p.getFullName("508", "mouse");
+}
+
+main();    
+
+    
